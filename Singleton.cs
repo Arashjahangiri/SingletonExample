f99598a0540b09f3b0788eaa9a8a841bbbcec21c
Arash <a@a.com>
2024-04-24 17:07:23 +0330

    Added Singleton CLass to Project

---
+++ Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    public sealed class Singleton
+    {
+
+        public static Singleton _instance;
+        public static object _lock = new object();
+
+
+
+
+        private Singleton() {}
+
+
+        public static Singleton GetInstance()
+        {
+
+          if (_instance==null)
+            {
+                lock (_lock)
+                {
+
+                    if (_instance==null)
+                    {
+
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+
+        }
+
+
+
+
+    }//////////////End of class
+}
