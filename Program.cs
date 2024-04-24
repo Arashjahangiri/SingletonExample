f15b6b99c02c03e24a7cb0e32d57a167c2b80013
Arash <a@a.com>
2024-04-24 17:09:47 +0330

    use singleton class in program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿
+
+
+
+using SingletonExample;
+
+//We can not use from this method because we use from private constractors...
+//Singleton obj = new Singleton(); 
+
+
+Singleton _obj = Singleton.GetInstance();
